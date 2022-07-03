344d5c446b1fb36371b18ca2a5e4d864a0c8c0c4
tiennma2k23cvp <tiennma2k23@cvp.vn>
2022-07-03 19:28:15 +0700

    addfile

--- SS3/index.js
+++ SS3/index.js
@@ -13,13 +13,25 @@ regbtn.addEventListener("click", () => {
   if (localStorage.user) {
     tg = JSON.parse(localStorage.user);
   }
-  //   let tg = JSON.parse(localStorage.getItem("user"));
-  let userInfo = {
-    email: email.value,
-    password: password.value,
-  };
-  tg.push(userInfo);
-  localStorage.setItem("user", JSON.stringify(tg));
+  let ok = false;
+  for (let index = 0; index < tg.length; index++) {
+    let user = tg[index];
+    if (user.email == email.value) {
+      ok = true;
+      break;
+    }
+  }
+  if (ok) alert("Đăng kí thật bại. Tài khoản đã tồn tại");
+  else {
+    //   let tg = JSON.parse(localStorage.getItem("user"));
+    let userInfo = {
+      email: email.value,
+      password: password.value,
+    };
+    tg.push(userInfo);
+    localStorage.setItem("user", JSON.stringify(tg));
+    alert("Đăng kí thành công");
+  }
 });
 let loginbtn = document.getElementById("login");
 loginbtn.addEventListener("click", () => {
@@ -27,7 +39,11 @@ loginbtn.addEventListener("click", () => {
     alert("Fail");
     exit();
   }
-  let userInfo = JSON.parse(localStorage.getItem("user"));
+  let userInfo = [];
+  if (localStorage.user) {
+    userInfo = JSON.parse(localStorage.user);
+  }
+  // let userInfo = JSON.parse(localStorage.getItem("user"));
   let ok = false;
   for (let index = 0; index < userInfo.length; index++) {
     let user = userInfo[index];
@@ -36,6 +52,6 @@ loginbtn.addEventListener("click", () => {
       break;
     }
   }
-  if (ok) alert("ACCEPT");
-  else alert("Fail");
+  if (ok) alert("Đăng nhập thành công");
+  else alert("Đăng nhập thất bại, vui lòng kiểm tra lại");
 });
